62c20b9212f98af8a623dffa3763e2c22b4d0b8e
SloCompTech <SloCompTech@users.noreply.github.com>
2016-11-30 18:22:18 +0100

    Created library
    Coded main code of program

---
+++ src/QList.h
@@ -0,0 +1,48 @@
+/**
+ * @file
+ * @author  SloCompTech <SloCompTech@gmail.com>
+ * @version 1.0
+ *
+ * @section DESCRIPTION
+ *
+ * This class enables to use linked lists
+ */
+
+#ifndef LIB_SCP_QLIST
+#define LIB_SCP_QLIST
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+template<typename T>
+class QList
+{
+private:
+  typedef struct node
+  {
+    T item;
+    node *next,*prev;
+  }node;
+
+  int len; //!< Size of list >
+  node *start,*end; //!< Pointers to start and end >
+
+public:
+  QList(); //!< Class constructor >
+  ~QList(); //!< Class destructor >
+
+  void push_back(const T i); //!< Push item at the back of list >
+  void push_front(const T i);//!< Push item at the front of the list >
+  void pop_back(); //!< Pops item from back >
+  void pop_front(); //!< Pops item from front >
+  T front(); //!< get item from front >
+  T back(); //!< get item from back >
+  int size(); //!< Returns size of list >
+  void clear(); //!< Clears list >
+  void clear(int index); //!< Clears list >
+  T get(int index); //!< Get item at given index >
+  T at(int index); //!< Get item at given index >
+
+};
+#endif
