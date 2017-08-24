6bfe64ac33f4680f03b79b0701aa9dc35eb91f0e
Martin Dagarin <SloCompTech@users.noreply.github.com>
2017-08-24 11:11:20 +0200

    Fixed comments

--- src/QList.cpp
+++ src/QList.cpp
@@ -129,7 +129,7 @@ void QList<T>::clear()
   {
     tmp = start;
     start = start->next;
-    delete tmp;
+    delete tmp; // Delete item
     len--; // Decrease counter
   }
   end = NULL;
@@ -153,7 +153,7 @@ void QList<T>::clear(unsigned int index)
         end = tmp->prev;
 
       len--; // Decrease counter
-      delete tmp; // fix for memory problem
+      delete tmp; // Delete item
       break;
     }
   }
